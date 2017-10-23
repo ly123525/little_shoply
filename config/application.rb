122c82e659b109461a40d23afeba0877cfa2bdc2
liuyang123525 <182160328@qq.com>
2017-10-23 23:47:23 +0800

    add css and js

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,9 @@ module LittleShoply
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |generator|
+      generator.assets false
+      generator.test_framework false
+    end
   end
 end
