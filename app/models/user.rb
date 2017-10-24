ae61ace843dc23fddf2eb8cec7ddb00bc2f66e20
liuyang123525 <182160328@qq.com>
2017-10-24 22:04:19 +0800

    add register

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,15 @@ class User < ApplicationRecord
   validates :password_confirmation, :presence=>{message: '确认密码不能为空'}, if: :need_validate_password
   validates :password, :confirmation=>{message: '两次密码不一致'}, if: :need_validate_password
 
+  #登陆后显示的
+  def user_name
+    self.email.split("@").first
+  end
+  #登陆后图表图标显示的
+  def user_name_icon
+    self.email[0]
+  end
+
   private
   def need_validate_password
     self.new_record? || (!self.password.nil?|| !self.password_confirmation.nil?)
